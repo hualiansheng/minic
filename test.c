5f456d80a5b0534119ed82f7463c65a161e0be32
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-10-03 13:12:16 +0000

    a sample for testing

---
+++ test.c
@@ -0,0 +1,19 @@
+int* fibonacci(int n)
+{
+	if (n == 1 || n == 0)
+		return 1;
+	return fibonacci(n-1) + fibonacci(n-2);
+}
+int main(int argc, char* argv)
+{
+	int i, k, a[10], b;
+	i = + - - + 0;
+	b = *x * *y;
+	while (i++ < 10)
+	{
+		b = fibonacci(b);
+		a[k=i] = b++ + --i;
+		i = !(i && j);
+	}
+	return 0;
+}
