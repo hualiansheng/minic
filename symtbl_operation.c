c87e35722088401c212c1a0314dbdf61b67a25bf
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-10-05 22:42:04 +0000

    symbol table partly done

--- symtbl_operation.c
+++ symtbl_operation.c
@@ -1,16 +1,21 @@
 #include "symtbl.h"
+#include "AST.h"
+
 symtbl_hdr* init_tbl()
 {
 	symtbl_hdr* p = (symtbl_hdr*)malloc(sizeof(symtbl_hdr));
 	assert(p != NULL);
+	p->ret_type = VOID_T;
+	p->ret_star = 0;
+	p->para_num = 0;
 	p->item_num = 0;
-	p->maxSize = 8;
-	p->item = (symtbl_item*)malloc(p->maxSize*sizeof(symtbl_item));
+	p->maxSize = 8 * sizeof(symtbl_item);
+	p->item = (symtbl_item*)malloc(p->maxSize);
 	assert(p->item != NULL);
 	return p;
 }
 
-int adjustSize(symtbl_hdr* p_tbl)
+/*int adjustSize(symtbl_hdr* p_tbl)
 {
 	int i;
 	symtbl_item* p_new = (symtbl_item*)malloc(2*p_tbl->maxSize*sizeof(symtbl_item));
@@ -21,12 +26,23 @@ int adjustSize(symtbl_hdr* p_tbl)
 	free(p_tbl->item);
 	p_tbl->item = p_new;
 	return 0;
+}*/
+
+int adjustSize(void* p_old, int* max)
+{
+	void* p_new = (void*)malloc((*max)*2+1);
+	assert(p_new != NULL);
+	strncpy(p_new, p_old, *max);
+	*max = *max * 2 + 1;
+	free(p_old);
+	p_old = p_new;
+	return 0;
 }
 
 int add_var_item(AST_NODE* p, symtbl_hdr* p_tbl, int type)
 {
-	if (p_tbl->item_num == p_tbl->maxSize)
-		adjustSize(p_tbl);
+	if ((p_tbl->item_num+1)*sizeof(syntbl_item) >= p_tbl->maxSize)
+		adjustSize((void*)(p_tbl->item), &(p_tbl->maxSize));
 	p = p->leftChild;
 	if (p->nodeType == ARRAY_VAR)
 	{
@@ -34,7 +50,7 @@ int add_var_item(AST_NODE* p, symtbl_hdr* p_tbl, int type)
 		p_tbl->(item+p_tbl->item_num)->star_num = 0;
 		p_tbl->(item+p_tbl->item_num)->writable = 0;
 		p = p->leftChild;
-		p_tbl->(item+p_tbl->item_num)->name = ;
+		p_tbl->(item+p_tbl->item_num)->name = name_address((p->content).s_content);
 		p_tbl->(item+p_tbl->item_num)->size = (p->rightSibling->rightSibling->content).i_content;
 	}
 	else
@@ -42,7 +58,7 @@ int add_var_item(AST_NODE* p, symtbl_hdr* p_tbl, int type)
 		if (p->nodeType == STAR)
 		{
 			p_tbl->(item+p_tbl->item_num)->star_num = 1;
-\			p = p->rightSibling;
+			p = p->rightSibling;
 		}
 		else
 			p_tbl->(item+p_tbl->item_num)->star_num = 0;
@@ -51,24 +67,34 @@ int add_var_item(AST_NODE* p, symtbl_hdr* p_tbl, int type)
 		{
 			p_tbl->(item+p_tbl->item_num)->type = type;
 			p_tbl->(item+p_tbl->item_num)->writable = 1;
-			p_tbl->(item+p_tbl->item_num)->name = ;
+			p_tbl->(item+p_tbl->item_num)->name = name_address((p->content).s_content);
 			p_tbl->(item+p_tbl->item_num)->size = -1;
 		}
 	}
 	p_tbl->item_num++;
 	return 0;
 }
+
 int add_func_item(AST_NODE* p, symtbl_hdr* p_tbl)
 {
-	int i, type = INT_T;
-	if (p_tbl->item_num == p_tbl->maxSize)
-		adjustSize(p_tbl);
-	if (p->nodeType == TYPE_NAME)
-	{
-		type = p->leftChild->nodeType;
-		p = p->rightSibling;
-	}
-	p_tbl->(item+p_tbl->item_num)->ret_type = type;
+	if ((p_tbl->item_num+1)*sizeof(syntbl_item) >= p_tbl->maxSize)
+		adjustSize((void*)(p_tbl->item), &(p_tbl->maxSize));
+	p_tbl->(item+p_tbl->item_num)->type = FUNCTION_DEF;
+	p_tbl->(item+p_tbl->item_num)->star_num = 0;
+	p_tbl->(item+p_tbl->item_num)->writable = 0;
+	p_tbl->(item+p_tbl->item_num)->name = name_address((p->content).s_content);
+	p_tbl->(item+p_tbl->item_num)->size = -1;
+	p_tbl->item_num++;
+	return 0;
+}
+
+int add_para_item(AST_NODE* p, symtbl_hdr* p_tbl)
+{
+	if ((p_tbl->item_num+1)*sizeof(syntbl_item) >= p_tbl->maxSize)
+		adjustSize((void*)(p_tbl->item), &(p_tbl->maxSize));
+	p = p->leftChild;
+	p_tbl->(item+p_tbl->item_num)->type = p->leftChild->nodeType;
+	p = p->rightSibling;
 	if (p->nodeType == STAR)
 	{
 		p_tbl->(item+p_tbl->item_num)->star_num = 1;
@@ -76,7 +102,33 @@ int add_func_item(AST_NODE* p, symtbl_hdr* p_tbl)
 	}
 	else
 		p_tbl->(item+p_tbl->item_num)->star_num = 0;
-	for ()
+	p_tbl->(item+p_tbl->item_num)->writable = 1;
+	p_tbl->(item+p_tbl->item_num)->name = name_address((p->content).s_content);
+	p_tbl->(item+p_tbl->item_num)->size = -1;	
 	p_tbl->item_num++;
+	p_tbl->para_num++;	
 	return 0;
+}
+
+int add_para_list(AST_NODE* p, symtbl_hdr* p_tbl)
+{
+	AST_NODE* ptr;
+	for (ptr = p->leftChild; ptr != NULL; ptr = ptr->leftChild)
+	{
+		if (ptr->rightSibling == NULL)
+			add_para_item(ptr, p_tbl);
+		else
+			add_para_item(ptr->rightSibling->rightSibling, p_tbl);
+	}
+	return 0;
+}
+
+char* name_address(char* ident_name)
+{
+	if (name_off+strlen(ident_name) >= name_size)
+		adjustSize((void*)name_pool, &name_size);
+	strncpy(name_pool+name_off, ident_name, strlen(ident_name));
+	name_off += strlen(ident_name)+1;
+	name_pool[name_off-1] = '\0';
+	return name_pool+name_off;
 }
