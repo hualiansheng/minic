035e1825ba8b53535302b0ff122b669f01ec050f
brillsp <brillsp@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-10-12 11:14:44 +0000

    修改main.c使minic支持命令行参数，说明见README
    原main.c被重命名为main.c.old，在Windows下编译需要它替换main.c(不支持参数)

--- main.c
+++ main.c
@@ -1,22 +1,89 @@
+/**
+ * Caution:
+ * Due to the dependency on unistd.h, 
+ * only under POSIX OS can this file be compiled.
+ * To use minic under Windows, compile main.c.old
+ */
 #include "validation_utils.h"
 #include "main.h"
 #include <assert.h>
-
+#include <unistd.h> //getopt() support
+void usage()
+{
+	printf("MiniC compiler:\n\
+Usage: minic [option] source_file\n\
+Options:\n\
+	-d Bison parser debug output\n\
+	-t Print AST\n\
+	-s Print symbol tables\n\
+	-a All of above = -tds\n");
+}
 int main(int argc, char** argv)
 {	
+	//CLI arguments
+	int oc,
+		dbg_print_tree = 0,
+		dbg_print_symtbl = 0,
+		dbg_type_check = 0;
+	FILE* source_file;
 	//symtable support: scope number
 	scope_number = 0;
 	//yydebug = 1;
-	FILE* source_file;
-	argc--,argv++;
-	if(argc)
-		source_file = fopen (argv[0], "r" );
-	assert( source_file != NULL );
+	while((oc = getopt(argc, argv, "dtsca")) != -1)
+	{
+		switch(oc)
+		{
+			case 'd': // debug on
+				yydebug = 1;
+				break;
+			case 't': //output AST
+				dbg_print_tree = 1;
+				break;
+			case 's':
+				dbg_print_symtbl = 1;
+				break;
+			case 'c':
+				dbg_type_check = 1;
+				break;
+			case 'a':
+				yydebug = dbg_print_tree
+					= dbg_print_symtbl 
+					= dbg_type_check 
+					= 1;
+				break;
+			default:
+				usage();
+				return -1;
+		}
+	}
+	if(optind >= argc) // no file name given
+	{
+		usage();
+		return -1;
+	}
+	
+	source_file = fopen (argv[optind], "r");
+	if (!source_file) //file open error
+	{
+		fprintf(stderr,"%s: open error!\n",argv[optind]);
+		return -1;
+	}
 	yyin = source_file;
+	//call bison parser
 	yyparse();
-	fclose(source_file);
-	//print_AST(tree_root, 0);
+	//generate symbol table
 	gen_symtbl(tree_root);
-	print_symtbl(tree_root);
+	/*
+	 * TODO: next compilation step
+	 */
+	if(dbg_print_tree)
+		print_AST(tree_root,0);
+	if(dbg_print_symtbl)
+		print_symtbl(tree_root);
+	if(dbg_type_check)
+		/*
+		 * TODO: type checking debug output.
+		 */
+		;
 	return 0;
 }
