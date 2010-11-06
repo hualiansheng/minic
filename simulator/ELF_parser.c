fa817e0d5de308c794a423476e3a7ffb6a23e406
yzt356 <yzt356@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-11-06 07:53:19 +0000

    ELF_parser done.

--- simulator/ELF_parser.c
+++ simulator/ELF_parser.c
@@ -1,75 +1,72 @@
-#include <err.h>
-#include <fcntl.h>
-#include <libelf.h>
-#include <gelf.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <sysexits.h>
-#include <unistd.h>
+#include <fcntl.h>
 
-int i, fd;
+#include <gelf.h>
 
-Elf *e;
-char *id, bytes[5];
-size_t n;
 
-GElf_Phdr phdr;
+int fd;
+Elf *e;  
+GElf_Ehdr ehdr;
+int phdr_index;
 
-void print_ptype ( size_t pt )
-{
-  char * s ;
-# define C(V) case PT_##V: s = #V; break
-    switch ( pt ) {
-      C(NULL);
-      C(LOAD);
-      C(INTERP);
-      C(NOTE);
-      C(PHDR);
-      C(TLS);
-      C(SUNWBSS);
-      C(SUNWSTACK);
-    default:
-      s =  "unknown" ;
-      break;
-    }
-    ( void ) printf ( " \"%s\" " , s );
-# undef C
+
+int Elf_initial(char *input_file){
+  if(elf_version(EV_CURRENT) == EV_NONE){
+    fprintf(stderr, "ELF library initialization failed: %s.\n",
+	    elf_errmsg(-1));
+    return 1;
+  }
+  if((fd = open(input_file, O_RDONLY, 0)) < 0){
+    fprintf(stderr, "Open \"%s\" failed.\n", input_file);
+    return 2;
+  }
+  if((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL){
+    fprintf(stderr, "elf_begin() failed: %s.\n", elf_errmsg(-1));
+    return 3;
+  }
+  if(elf_kind(e) != ELF_K_ELF){
+    fprintf(stderr, "\"%s\" is not an ELF object.\n", input_file);
+    return 4;
+  }
+  if(gelf_getehdr(e, &ehdr) == NULL){
+    fprintf(stderr, "getehdr() failed: %s.\n",
+	    elf_errmsg(-1));
+    return 5;
+  }
+  if(ehdr.e_flags != 2){
+    fprintf(stderr, "\"%s\" is not an execution file.\n", input_file);
+    return 6;
+  }
+
+  phdr_index = 0;
+
+  // printf("Entry point: 0x%jx\n", ehdr.e_entry);
+  // printf("Flags: %d\n", ehdr.e_flags);
+  return 0;
 }
 
-
-void Elf_parser(char *input_path){
-  if(elf_version(EV_CURRENT) == EV_NONE)
-    errx(EX_SOFTWARE, "ELF library initialization failed: %s", 
-	 elf_errmsg(-1));
-
-  if((fd = open(input_path, O_RDONLY, 0)) < 0)
-    err(EX_NOINPUT, "open \"%s\" failed", input_path);
-
-  if((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
-    errx(EX_SOFTWARE, "elf_begin() failed: %s.",
-	 elf_errmsg(-1));
-
-  if(elf_kind(e) != ELF_K_ELF)
-    errx(EX_DATAERR, "\"%s\" is not an ELF object.", input_path);
-  
-  if ( elf_getphdrnum (e , &n ) != 0)
-    errx ( EX_DATAERR , " elf_getphdrnum () failed : %s . " ,
-	   elf_errmsg ( -1));
-  
-  for(i=0;i<n;i++){
-    if(gelf_getphdr(e, i, &phdr)!=&phdr)
-      errx(EX_SOFTWARE, "getphdr() failed: %s.",
-	   elf_errmsg(-1));
-    printf("PHDR %d:\n",i);
-    printf("    p_type 0x%jx ", (uintmax_t)phdr.p_type);
-    print_ptype(phdr.p_type);
-    printf("\n");
-    printf("    p_offset 0x%jx \n", (uintmax_t)phdr.p_offset);
-    printf("    p_vaddr 0x%jx \n", (uintmax_t)phdr.p_vaddr);
-    printf("    p_paddr 0x%jx \n", (uintmax_t)phdr.p_paddr);
-  }
+int Elf_close(){
   elf_end(e);
   close(fd);
-  exit(EX_OK);
+}
+
+//get next loadable program header
+int Elf_next_loadable_phdr(GElf_Phdr *phdr){
+  int phdr_num = ehdr.e_phnum;
+  while(phdr_index < phdr_num){
+    if(gelf_getphdr(e, phdr_index, phdr) != phdr){
+      fprintf(stderr, "getphdr() failed: %s.", elf_errmsg(-1));
+      return 0;
+    }
+    phdr_index ++;
+    if(phdr->p_type == PT_LOAD)
+      return 1;
+  }
+  phdr_index = 0;
+  return 0;
+}
+
+//get elf file entry address
+uint32_t Elf_entry_point(){
+  return ehdr.e_entry;
 }
--- simulator/simulator.c
+++ simulator/simulator.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <err.h>
-#include <sysexits.h>
+#include <gelf.h>
 
 int main(int argc, char **argv){
   if(argc !=2)
-    errx(EX_USAGE, "usage: %s file-name", argv[0]);
-  Elf_parser(argv[1]);
+    fprintf(stderr, "usage: %s file-name", argv[0]);
+  Elf_initial(argv[1]);
+  Elf_close();
+  GElf_Phdr phdr;
+  if(Elf_next_loadable_phdr(&phdr) != 0)
+    printf("Phdr type : %d\n", phdr.p_type);
   return 0;
 }
