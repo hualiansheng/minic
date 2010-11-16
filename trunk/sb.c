6df87f11bbd81776fa317cb8ff260c014d7412bd
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-11-16 05:18:38 +0000

    xxx

--- trunk/sb.c
+++ trunk/sb.c
@@ -2,10 +2,11 @@ int f(int a){
 	if(a<=1) return 1;
 	return f(a-1)+f(a-2);
 }
-
-int main(){
-	int a;
-	a = 5;
-	a = f(a);
-	return 1;
+int main()
+{
+	int a, b;
+	char k(char a);
+	int f(int c, int d), h(char s);
+	k(f(a,b));
+	return h(b); 
 }
