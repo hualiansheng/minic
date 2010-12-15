a3982c12779c6db240502e7e063e0d8bb1ee426e
yzt356 <yzt356@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-15 13:31:27 +0000

    Verification examples

--- simulator/verification/simulator/qsort.c
+++ simulator/verification/simulator/qsort.c
@@ -1,7 +1,11 @@
+extern void print_int(int i);
+extern void print_char(char c);
+extern void print_string(char* s);
+
 void qsort(int *data,int begin, int end){
+  int i, j, tmp;
   if(end <= begin + 1)
     return;
-  int i, j, tmp;
   j=begin+1;
   for(i=begin+1; i<end; i++){
     if(data[i] < data[begin]){
@@ -16,13 +20,20 @@ void qsort(int *data,int begin, int end){
   data[j-1] = tmp;
   qsort(data,begin, j-1);
   qsort(data,j, end);
+  return;
 }
 
 int main(){
   int i;
-  int data[20];
-  for(i=0; i<20; i++)
-    data[i] = 20-i;
-  qsort(data,0, 20);
+  int data[128];
+  for(i=0; i<128; i++)
+    data[i] =128-i;
+  qsort(data,0, 128);
+  print_string("The result is:\n");
+  for(i=0; i<128; i++){
+    print_int(data[i]);
+    print_char(' ');
+  }
+  print_string("\n");
   return 0;
 }
