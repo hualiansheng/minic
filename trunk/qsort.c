82e1f195fafebd7918fd7ab688a327918135cef0
brillsp <brillsp@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-15 05:21:02 +0000

    qsort.c

---
+++ trunk/qsort.c
@@ -0,0 +1,28 @@
+void qsort(int *data,int begin, int end){
+  int i, j, tmp;
+  if(end <= begin + 1)
+    return;
+  j=begin+1;
+  for(i=begin+1; i<end; i++){
+    if(data[i] < data[begin]){
+      tmp = data[j];
+      data[j] = data[i];
+      data[i] = tmp;
+      j++;
+    }
+  }
+  tmp = data[begin];
+  data[begin] = data[j-1];
+  data[j-1] = tmp;
+  qsort(data,begin, j-1);
+  qsort(data,j, end);
+}
+
+int main(){
+  int i;
+  int data[20];
+  for(i=0; i<20; i++)
+    data[i] = 20-i;
+  qsort(data,0, 20);
+  return 0;
+}
