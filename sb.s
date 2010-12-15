9b6d4950d35e84eeb075102b74952a742388c744
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-15 15:58:12 +0000

    bool assign supported

--- sb.s
+++ sb.s
@@ -1,58 +1,46 @@
 main:
-	stw	r31, [r29+], #-4
-	stw	r30, [r29+], #-8
-	stw	r29, [r29+], #-12
-	stw	r27, [r29+], #-16
-	sub	r27, r29, #4
-	sub	r29, r29, #544
-	add	r3, r27, #-528
-	stw	r3, [r27+], #-532
-	mov	r5, #0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	ldw	r15, .L11
+	@ lr needed for prologue
+	ldw	r15, [r15+], #0
+	cmpsub.a	r15, #0
+	beq	.L2
+	ldw	r14, .L11+4
+	ldw	r13, [r14+], #0
 .L3:
-	cmpsub.a	r5, #128
-	beg	.L2
-.L1:
-	rsub	r6, r5, #128
-	ldw	r4, [r27+], #-532
-	stw	r6, [r4+], r5<<#2
-	add	r5, r5, #1
-	b	.L3
-.L2:
-	mov	r5, #0
-.L11:
-	cmpsub.a	r5, #128
-	beg	.L5
-.L4:
-	add	r6, r5, #1
-	mov	r9, r6
-.L10:
-	cmpsub.a	r9, #128
-	beg	.L7
-.L6:
-	ldw	r3, [r4+], r9<<#2
-	mov	r7, r3
-	ldw	r3, [r4+], r5<<#2
-	mov	r6, r3
-	cmpsub.a	r7, r6
-	beg	.L9
-.L8:
-	ldw	r3, [r4+], r5<<#2
-	mov	r6, r3
-	mov	r8, r6
-	ldw	r3, [r4+], r9<<#2
-	mov	r6, r3
-	stw	r6, [r4+], r5<<#2
-	stw	r8, [r4+], r9<<#2
-.L9:
-	add	r6, r9, #1
-	mov	r9, r6
-	b	.L10
-.L7:
-	add	r5, r5, #1
-	b	.L11
+	cmpsub.a	r15, r13
+	bsg	.L5
+	bsl	.L6
 .L5:
-	mov	r0, #0
-	ldw	r30, [r27+], #-4
-	ldw	r29, [r27+], #-8
-	ldw	r27, [r27+], #-12
-	jump	r30
+	cmpsub.a	r13, #0
+	bne	.L6
+	cmpsub.a	r15, #0
+	beq	.L10
+.L6:
+	mov	r0, #2
+	stw	r0, [r14+], #0
+	jump	lr
+.L2:
+	ldw	r14, .L11+4
+	ldw	r13, [r14+], #0
+	cmpadd.a	r13, #1
+	bne	.L3
+	mov	r15, #3
+	mov	r0, r15
+	stw	r15, [r14+], #0
+	jump	lr
+.L10:
+	mov	r0, #1
+	stw	r0, [r14+], #0
+	jump	lr
+.L12:
+	.align	2
+.L11:
+	.word	a
+	.word	b
+	.size	main, .-main
+	.comm	a, 4, 4
+	.comm	b, 4, 4
+	.ident	"GCC: (UC4_1.0_gama_20101126) 4.4.2"
