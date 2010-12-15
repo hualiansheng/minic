a7a7f68bfb4f2a3ac719d39030a8781638294cce
brillsp <brillsp@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-15 05:18:33 +0000

    中间代码rvalue Bug fix

--- sb.c
+++ sb.c
@@ -3,18 +3,14 @@ int main()
 	int i, j, a[10], temp;
 	for (i = 0; i < 10; i++)
 		a[i] = 10 - i;
-	for (i = 1; i < 10; i++)
-	{
-		for (j = 0; j < 10-i; j++)
-		{
-			if (a[j] > a[j+1])
+	for (i = 0; i < 10; i++)
+		for (j = i+1; j < 10; j++)
+			if(a[j] < a[i])
 			{
-				temp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
 			}
-		}			
-	}
 	return 0;
 }
 
