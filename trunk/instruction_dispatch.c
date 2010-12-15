b2b034deba3a56f741ea39b070954c08380376b4
Kerrywang881122 <Kerrywang881122@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-15 01:28:00 +0000

    instruction_dispatch.c完成, 没有目标代码划块

---
+++ trunk/instruction_dispatch.c
@@ -0,0 +1,168 @@
+#include "register.h"
+extern int *instruction_blocks;
+extern int instruction_block_num;
+extern assamble *assamble_list;
+extern int assemble_num;
+int **data_dep_graph;// for each block
+int *key_path; // lenth of longest key_path
+int *assamble_dispatch_index; // array of dispatch
+int inst_num_block; //instruction number in current block
+int *dispatch_block; // array of dispatch in current block
+int current_inst; // the current instruction number in the dispatch block;
+int *in_degree; // array of in degree
+
+
+void topo_sort(int begin, int end);
+void create_dep_graph(int begin, int end);
+void do_first(int x);
+
+void instruction_dispatch()
+{
+	int i;
+	assamble_dispatch_index = malloc(sizeof(int) * assemble_num);
+	for(i = 0 ; i < assemble_num ; i++)	assamble_dispatch_index[i] = i;
+	for(i = 0 ; i < instruction_block_num ; i++){
+		create_dep_graph(instruction_blocks[i*2], instruction_blocks[i*2+1]);
+		find_key_path();
+		topo_sort(instruction_blocks[i*2],instruction_blocks[i*2+1]);
+	}
+}
+
+void create_dep_graph(int begin, int end)
+{
+	int i, j;
+	assamble temp;
+	inst_num_block = end - begin + 1;
+	data_dep_graph = (int **)malloc(sizeof(int *)*inst_num_block);
+	for(i = 0 ; i < inst_num_block ; i++){
+		data_dep_graph[i] = malloc(sizeof(int) * inst_num_block);
+		memset(data_dep_graph[i],0,sizeof(int)* inst_num_block);
+	}
+	for(i = begin ; i <= end; i++){
+		temp = assamble_list[i];
+		switch (temp.ins){
+			case ldw:{
+						 for(j = begin; j < i; j++){
+							if(assamble_list[j].ins == ldw || assamble_list[j].ins == stw) data_dep_graph[j][i] = 1;
+							else if(assamble_list[j].Rn != -1 && assamble_list[j].Rn == temp.Rd) data_dep_graph[j][i] = 1;
+							else if(assamble_list[j].Rs_or_Imm == 1 && assamble_list[j].Rs != -1 && assamble_list[j].Rs == temp.Rd) data_dep_graph[j][i] = 1;
+							else if(assamble_list[j].Rm_or_Imm == 1 && assamble_list[j].Rm != -1 && assamble_list[j].Rm == temp.Rd) data_dep_graph[j][i] = 1;						 
+						 }
+						 for(j = i + 1 ; j <= end; j++){
+						 	if(assamble_list[j].ins == ldw || assamble_list[j].ins == stw) data_dep_graph[i][j] = 1;
+							else if(assamble_list[j].Rn != -1 && assamble_list[j].Rn == temp.Rd) data_dep_graph[i][j] = 1;
+							else if(assamble_list[j].Rs_or_Imm == 1 && assamble_list[j].Rs != -1 && assamble_list[j].Rs == temp.Rd) data_dep_graph[i][j] = 1;
+							else if(assamble_list[j].Rm_or_Imm == 1 && assamble_list[j].Rm != -1 && assamble_list[j].Rm == temp.Rd) data_dep_graph[i][j]= 1;
+						 }
+					 }
+			case stw:{
+						 for(j = begin ; j < i ; j++){
+							if(assamble_list[j].ins == ldw || assamble_list[j].ins == stw) data_dep_graph[j][i] = 1;						 
+						 }
+						 for(j = i+1 ; j <= end; j++){
+						 	if(assamble_list[j].ins == ldw || assamble_list[j].ins == stw) data_dep_graph[i][j] = 1;
+						 }
+					 }
+			default:{
+						if(temp.Rd != -1){
+							for(j = begin ; j < i ; j++){
+								if(assamble_list[j].Rn != -1 && assamble_list[j].Rn == temp.Rd) data_dep_graph[j][i] = 1;
+								else if(assamble_list[j].Rs_or_Imm == 1 && assamble_list[j].Rs != -1 && assamble_list[j].Rs == temp.Rd) data_dep_graph[j][i] = 1;
+								else if(assamble_list[j].Rm_or_Imm == 1 && assamble_list[j].Rm != -1 && assamble_list[j].Rm == temp.Rd) data_dep_graph[j][i] = 1;						 
+							}
+							for(j = i ; j <= end; j++){
+								if(assamble_list[j].Rn != -1 && assamble_list[j].Rn == temp.Rd) data_dep_graph[i][j] = 1;
+								else if(assamble_list[j].Rs_or_Imm == 1 && assamble_list[j].Rs != -1 && assamble_list[j].Rs == temp.Rd) data_dep_graph[i][j] = 1;
+								else if(assamble_list[j].Rm_or_Imm == 1 && assamble_list[j].Rm != -1 && assamble_list[j].Rm == temp.Rd) data_dep_graph[i][j]= 1;
+							}
+						}
+					}
+		}
+	}
+	//array of in_degree
+	in_degree = (int *)malloc(sizeof(int)*inst_num_block);
+	memset(in_degree, 0, sizeof(in_degree));
+	for(i = 0 ; i < inst_num_block; i ++){
+		for(j = 0 ; j < inst_num_block; j++)
+			in_degree[i] += data_dep_graph[j][i];	
+	}
+}
+/*
+void find_key_path()
+{
+	int i, j, degree, flag;
+	key_path = malloc(sizeof(int) * inst_num_block);
+	for(i = 0 ; i < inst_num_block; i++){
+		key_path[i] = -1;
+	}
+	for(i = inst_num_block - 1 ; i >= 0 ; i--){
+		degree = 0;
+		for(j = 0 ; j < inst_num_block ; j++) degree += data_dep_graph[i][j];
+		if(degree == 0) key_path[i] = 0;
+	}
+	flag = 1;
+	while(flag){
+		flag = 0;
+		for(i = 0 ; i < inst_num_block ; i++){
+			if(key_path[i] != -1){
+				for(j = 0 ; j < inst_num_block ; j++){
+					if(j == i ) continue;
+					if (data_dep_graph[j][i] != 0){
+						key_path[j] = key_path[i] + 1;
+						flag = 1;
+					}
+				}		
+			}
+		}	
+	}
+
+}
+*/
+void topo_sort(int begin , int end)
+{
+	int i, j, in_degree, flag = 1, temp_index = 0;
+	int k, t;
+	dispatch_block = malloc(sizeof(int)* inst_num_block);
+	memset(dispatch_block, 0, sizeof(dispatch_block));
+	current_inst = 0;
+	for(i = begin ; i <= end ; i ++){
+		if(assamble_list[i].ins == ldw){
+			do_first(i);
+			for(j = 0 ; j < inst_num_block ; j++){
+				if(in_degree[i] == 0 && data_dep_graph[i][j] ==0){
+					do_first(j);
+					break;
+				}
+			}
+		}	
+	}
+	flag = 1;
+	while(flag){
+		flag = 0;
+		for(i = begin; i <= end; i++){
+			if(in_degree[i] == 0){
+				do_first(i);
+				flag = 1;
+			}
+		}
+	}
+}
+
+void do_first(int x)
+{
+	int i;
+	for(i = 0 ; i < inst_num_block ; i++){
+		if(data_dep_graph[i][x] != 0){
+			do_first(i);		
+		}	
+	}
+	dispatch_block[current_inst++] = x;
+	//remove the node in the graph
+	for(i = 0 ; i < inst_num_block ; i++){
+		if(data_dep_graph[x][i] == 1){
+			in_degree[i] -= 1;
+			data_dep_graph[x][i] = 0;
+		}
+	}
+	in_degree[x] = -1;
+}
