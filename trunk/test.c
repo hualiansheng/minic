cf90f626df70944baf966f312a964ce981226661
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-17 12:59:20 +0000

    suiyi

--- trunk/test.c
+++ trunk/test.c
@@ -1,5 +1,19 @@
+int f(int x, int y)
+{
+	if (x == 0)
+		return 0;
+	return f(x-1, y-1) + f(x-2, y-2);
+}
+
 int main()
 {
-	int a[10];
-	a [1] = a[2];
+	int a, b, c;
+	int *p;
+	a = 1;
+	c = 2;
+	p = &b;
+	p = p + 1;
+	*p = 5;
+	c = f(a, c);
+	return c;
 }
--- trunk/test.s
+++ trunk/test.s
@@ -1,19 +1,54 @@
+	.file	"test.c"
+	.text
+	.align	2
+	.global	f
+	.type	f,function
+f:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	stw	lr, [sp-], #4;
+	stw	sl, [sp-], #8;
+	stw	r24, [sp-], #12;
+	stw	r24, [sp-], #12;stw	r23, [sp-], #16;
+	stw	r24, [sp-], #12;stw	r23, [sp-], #16;sub	sp, sp, #16;
+	sub	r24, r1, #1
+	sub.a	sl, r0, #0
+	mov	r23, #0
+	beq	.L3
+.L6:
+	sub	r0, sl, #1
+	mov	r1, r24
+	b.l	f
+	sub.a	sl, sl, #2
+	add	r23, r23, r0
+	sub	r24, r24, #2
+	bne	.L6
+.L3:
+	mov	r0, r23
+	ldw.w	r23,  [sp]+, #4
+	ldw.w	r24,  [sp]+, #4
+	ldw.w	sl,  [sp]+, #4
+	ldw.w	ip,  [sp]+, #4
+	jump	ip
+	.size	f, .-f
+	.align	2
 	.global	main
-	.type main, function
+	.type	main,function
 main:
-	stw	r31, [r29+], #-4
-	stw	r30, [r29+], #-8
-	stw	r29, [r29+], #-12
-	stw	r27, [r29+], #-16
-	sub	r27, r29, #4
-	sub	r29, r29, #24
-	b.l	rand_int
-	mov	r4, r0
-	mov	r0, r4
-	b.l	print_int
-	mov	r0, #0
-.L1:
-	ldw	r30, [r27+], #-4
-	ldw	r29, [r27+], #-8
-	ldw	r27, [r27+], #-12
-	jump	r30
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	mov	r0, #1
+	mov	r1, #2
+	@ lr needed for prologue
+	@ Sibcall epilogue
+	b	f
+	.size	main, .-main
+	.global	k
+	.data
+	.align	2
+	.type	k, object
+	.size	k, 4
+k:
+	.word	1
+	.ident	"GCC: (UC4_1.0_gama_20101126) 4.4.2"
