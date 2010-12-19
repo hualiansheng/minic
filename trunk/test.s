b3d4dc052deaf917124d03dd04b2283957757c89
Kerrywang881122 <Kerrywang881122@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-19 06:24:20 +0000

    hehe

--- trunk/test.s
+++ trunk/test.s
@@ -1,54 +1,31 @@
-	.file	"test.c"
 	.text
-	.align	2
-	.global	f
-	.type	f,function
-f:
-	@ args = 0, pretend = 0, frame = 0
-	@ frame_needed = 0, uses_anonymous_args = 0
-	stw	lr, [sp-], #4;
-	stw	sl, [sp-], #8;
-	stw	r24, [sp-], #12;
-	stw	r24, [sp-], #12;stw	r23, [sp-], #16;
-	stw	r24, [sp-], #12;stw	r23, [sp-], #16;sub	sp, sp, #16;
-	sub	r24, r1, #1
-	sub.a	sl, r0, #0
-	mov	r23, #0
-	beq	.L3
-.L6:
-	sub	r0, sl, #1
-	mov	r1, r24
-	b.l	f
-	sub.a	sl, sl, #2
-	add	r23, r23, r0
-	sub	r24, r24, #2
-	bne	.L6
-.L3:
-	mov	r0, r23
-	ldw.w	r23,  [sp]+, #4
-	ldw.w	r24,  [sp]+, #4
-	ldw.w	sl,  [sp]+, #4
-	ldw.w	ip,  [sp]+, #4
-	jump	ip
-	.size	f, .-f
-	.align	2
 	.global	main
 	.type	main,function
 main:
-	@ args = 0, pretend = 0, frame = 0
-	@ frame_needed = 0, uses_anonymous_args = 0
-	@ link register save eliminated.
-	mov	r0, #1
-	mov	r1, #2
-	@ lr needed for prologue
-	@ Sibcall epilogue
-	b	f
-	.size	main, .-main
-	.global	k
-	.data
-	.align	2
-	.type	k, object
-	.size	k, 4
-k:
-	.word	1
-	.ident	"GCC: (UC4_1.0_gama_20101126) 4.4.2"
+	stw	r31, [r29+], #-4
+	stw	r30, [r29+], #-8
+	stw	r29, [r29+], #-12
+	stw	r27, [r29+], #-16
+	sub	r27, r29, #4
+	sub	r29, r29, #27
+	add	r3, r27, #-19
+	stw	r3, [r27+], #-23
+	ldw	r5, [r27+], #-23
+	mov	r7, #0
+.L3:
+	cmpsub.a	r7, #10
+	beg	.L4
+.L1:
+	add	r6, r5, r7
+	mov	r4, #122
+	sub	r4, r4, r7
+	stb	r4, [r6+], #0
+	add	r4, r7, #1
+	mov	r7, r4
+	b	.L3
+.L4:
+	ldw	r30, [r27+], #-4
+	ldw	r29, [r27+], #-8
+	ldw	r27, [r27+], #-12
+	jump	r30
+.L5:
