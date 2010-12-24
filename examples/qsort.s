2c5e6179adde6f1cd9ec6ae9f8b2efe06454e9db
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-24 02:39:23 +0000

    tail recursion optimization

--- examples/qsort.s
+++ examples/qsort.s
@@ -8,32 +8,30 @@ qsort:
 	stw	r27, [r29+], #-16
 	sub	r27, r29, #4
 	sub	r29, r29, #44
-	stw	r0, [r27+], #-28
-	stw	r1, [r27+], #-24
-	stw	r2, [r27+], #-20
-	ldw	r6, [r27+], #-20
-	ldw	r5, [r27+], #-24
-	ldw	r4, [r27+], #-28
+	mov	r4, r0
+	mov	r5, r1
+	mov	r6, r2
+.L1:
 	add	r8, r5, #1
 	cmpsub.a	r6, r8
-	bsg	.L2
-.L1:
-	b	.L8
+	bsg	.L3
 .L2:
+	b	.L9
+.L3:
 	mov	r13, r8
 	mov	r12, r8
-.L7:
+.L8:
 	cmpsub.a	r12, r6
-	beg	.L4
+	beg	.L5
 	ldw	r11, [r27+], #-40
-.L3:
+.L4:
 	add	r9, r4, r12<<#2
 	ldw	r8, [r9+], #0
 	add	r7, r4, r5<<#2
 	ldw	r7, [r7+], #0
 	cmpsub.a	r8, r7
-	beg	.L6
-.L5:
+	beg	.L7
+.L6:
 	add	r8, r4, r13<<#2
 	ldw	r7, [r8+], #0
 	mov	r11, r7
@@ -42,15 +40,15 @@ qsort:
 	stw	r11, [r9+], #0
 	add	r7, r13, #1
 	mov	r13, r7
-.L6:
+.L7:
 	add	r7, r12, #1
 	mov	r12, r7
-	b	.L7
-.L4:
+	b	.L8
+.L5:
 	add	r10, r4, r5<<#2
 	ldw	r7, [r10+], #0
-	mov	r11, r7
 	sub	r9, r13, #1
+	mov	r11, r7
 	add	r8, r4, r9<<#2
 	ldw	r7, [r8+], #0
 	stw	r7, [r10+], #0
@@ -71,16 +69,14 @@ qsort:
 	ldw	r13, [r27+], #-32
 	ldw	r12, [r27+], #-36
 	ldw	r11, [r27+], #-40
-	mov	r0, r4
-	mov	r1, r13
-	mov	r2, r6
-	b.l	qsort
-.L8:
+	mov	r5, r13
+	b	.L1
+.L9:
 	ldw	r30, [r27+], #-4
 	ldw	r29, [r27+], #-8
 	ldw	r27, [r27+], #-12
 	jump	r30
-.L9:
+.L10:
 	.text
 	.global	main
 	.type	main,function
@@ -90,27 +86,28 @@ main:
 	stw	r29, [r29+], #-12
 	stw	r27, [r29+], #-16
 	sub	r27, r29, #4
-	sub	r29, r29, #536
 	add	r3, r27, #-528
 	stw	r3, [r27+], #-532
 	ldw	r5, [r27+], #-532
+	sub	r29, r29, #536
+.L11:
 	mov	r7, #0
-.L12:
+.L14:
 	cmpsub.a	r7, #128
-	beg	.L11
-.L10:
+	beg	.L13
+.L12:
 	add	r6, r5, r7<<#2
 	rsub	r4, r7, #128
 	stw	r4, [r6+], #0
 	add	r4, r7, #1
 	mov	r7, r4
-	b	.L12
-.L11:
-	mov	r7, #0
-.L15:
-	cmpsub.a	r7, #128
-	beg	.L14
+	b	.L14
 .L13:
+	mov	r7, #0
+.L17:
+	cmpsub.a	r7, #128
+	beg	.L16
+.L15:
 	add	r4, r5, r7<<#2
 	ldw	r4, [r4+], #0
 	stw	r5, [r27+], #-532
@@ -127,8 +124,8 @@ main:
 	ldw	r5, [r27+], #-532
 	add	r4, r7, #1
 	mov	r7, r4
-	b	.L15
-.L14:
+	b	.L17
+.L16:
 	stw	r5, [r27+], #-532
 	stw	r7, [r27+], #-16
 	mov	r0, #10
@@ -148,10 +145,10 @@ main:
 	b.l	qsort
 	ldw	r5, [r27+], #-532
 	mov	r7, #0
-.L18:
+.L20:
 	cmpsub.a	r7, #128
-	beg	.L17
-.L16:
+	beg	.L19
+.L18:
 	add	r4, r5, r7<<#2
 	ldw	r4, [r4+], #0
 	stw	r5, [r27+], #-532
@@ -168,16 +165,16 @@ main:
 	ldw	r5, [r27+], #-532
 	add	r4, r7, #1
 	mov	r7, r4
-	b	.L18
-.L17:
+	b	.L20
+.L19:
 	mov	r0, #10
 	b.l	print_char
 	mov	r0, #10
 	b.l	print_char
 	mov	r0, #0
-.L19:
+.L21:
 	ldw	r30, [r27+], #-4
 	ldw	r29, [r27+], #-8
 	ldw	r27, [r27+], #-12
 	jump	r30
-.L20:
+.L22:
--- sb.c
+++ sb.c
@@ -1,10 +1,46 @@
-int a;
-int main()
-{
-	int b, num[10];
-	a = 1;
-	a = 2;
-	num[1] = 4;
-	num[1] = 3;
- 	return b;
+void print_int(int i);
+void print_char(char c);
+void print_string(char* s);
+int rand_int(int num);
+
+int qsort(int *data,int begin, int end){
+  int i, j, tmp;
+  if(end <= begin + 1)
+    return 0;
+  j=begin+1;
+  for(i=begin+1; i<end; i++){
+    if(data[i] < data[begin]){
+      tmp = data[j];
+      data[j] = data[i];
+      data[i] = tmp;
+      j++;
+    }
+  }
+  tmp = data[begin];
+  data[begin] = data[j-1];
+  data[j-1] = tmp;
+  qsort(data,begin, j-1);
+  qsort(data,j, end);
+  return 0;
+}
+
+int main(){
+  int i;
+  int data[128];
+  for(i=0; i<128; i++)
+      data[i] = 128 - i;
+  for(i=0; i<128; i++){
+    print_int(data[i]);
+    print_char(' ');
+  }
+  print_char('\n');
+  print_char('\n');
+  qsort(data,0, 128);
+  for(i=0; i<128; i++){
+    print_int(data[i]);
+    print_char(' ');
+  }
+  print_char('\n');
+  print_char('\n');
+  return 0;
 }
