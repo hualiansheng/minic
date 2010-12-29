b0195f6b6c59b62043e6b23408547cce1fab6c43
hualiansheng <hualiansheng@cd2967d3-85c2-84e9-6a56-6c2f9b57a5c7>
2010-12-29 18:42:35 +0000

    hehe

--- trunk/sb.c
+++ trunk/sb.c
@@ -1,26 +1,8 @@
-//1 2 3 4 6
-int a,b;
-int print_int(int x);
-int print_char(char x);
+int i;
 int main()
 {
-	int a[10],b,i,j,k;
-	i = 0;
-	j = 1;
-	b = i + j;
-	for(k = 0 ; k < 10; k++)
-	{
-		a[i+j] = i + j;
-		if(k<5)
-			i++;
-		else
-			b = i + j;
-		b = i + j;
-	}
-	for(i = 1 ; i < 5; i++)
-	{	
-		print_int(a[i]);
-		print_char('\n');
-	}
-	print_int(b);
+	int j;
+	for (j = 1 ; i < 100; j++)
+		i = i + j;
+	return 0;
 }
